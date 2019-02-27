e1c6cdc4c7edf2486a5ff31264f24d2bb1bfe5cf
baiye <m15810206306>
2019-02-27 15:14:52 +0800

    profession 业务代码中，增加了RecyclerView 自动滑动到某个条目，某个位置的功能

--- Template/profession/src/main/java/app/vp/cn/profession/MainActivity.java
+++ Template/profession/src/main/java/app/vp/cn/profession/MainActivity.java
@@ -1,13 +1,48 @@
 package app.vp.cn.profession;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import app.vp.cn.profession.adapter.RcvAdapter;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.rcv)
+    RecyclerView rcv;
+    @BindView(R.id.iv_back)
+    ImageView ivBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        rcv.setLayoutManager(new LinearLayoutManager(this));
+
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            list.add("自动滚动到指定位置" + i);
+        }
+        RcvAdapter rcvAdapter = new RcvAdapter(list);
+        rcv.setAdapter(rcvAdapter);
+    }
+
+    @OnClick(R.id.iv_back)
+    public void onViewClicked() {
+        //rcv.scrollToPosition(5); //滚动到指定位置   不带动画
+        rcv.smoothScrollToPosition(0);  // 滚动到指定位置 带滑动动画
+
+        // rcv.scrollBy(0, 200); //滚动到指定距离
+        // rcv.smoothScrollBy(0, 500);  //滚动到指定距离  带动画
     }
 }
---
+++ Template/profession/src/main/java/app/vp/cn/profession/adapter/RcvAdapter.java
@@ -0,0 +1,56 @@
+package app.vp.cn.profession.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import app.vp.cn.profession.R;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * author : by
+ * date: 2019/2/27 0027  下午 2:29.
+ * describe
+ */
+
+public class RcvAdapter extends RecyclerView.Adapter {
+
+    private List<String> list;
+
+    public RcvAdapter(List<String> list) {
+        this.list = list;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent,false);
+        return new ItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        ItemViewHolder itemViewHolder = (ItemViewHolder) holder;
+        itemViewHolder.tv_text.setText(list.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return list == null ? 0 : list.size();
+    }
+
+    class ItemViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.tv_text)
+        TextView tv_text;
+
+        public ItemViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+}
